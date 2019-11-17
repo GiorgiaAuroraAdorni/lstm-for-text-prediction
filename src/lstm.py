4eed5bc0e7af3e52794db8b3dfb103b96d8640d7
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-11-17 18:04:00 +0100

    Generated example and target

--- src/lstm.py
+++ src/lstm.py
@@ -48,6 +48,10 @@ def generate_batches(input, batch_size, sequence_length, counter):
     return batches
 
 
+########################################################################################################################
+
+
+# Download a large book from Project Gutenberg in plain English text
 download = False
 book = 'TheCountOfMonteCristo.txt'
 
@@ -70,24 +74,23 @@ with open(book, "r") as reader:
     abs_freq = collections.Counter(input)
     counter = len(abs_freq)
     abs_freq = collections.OrderedDict(abs_freq)
-
-    rel_freq = {key: value / counter for key, value in abs_freq.items()}
+    rel_freq = {key: value / len(input) for key, value in abs_freq.items()}
 
     # choose one integer to represent each character
     char_to_int = {key: idx for idx, key in enumerate(abs_freq)}
     int_to_char = {idx: key for idx, key in enumerate(abs_freq)}
 
-    integer_encoded = [char_to_int[char] for char in input]
+    encoded_input = [char_to_int[char] for char in input]
 
-    # One-hot encoding X_int
-    X = tf.one_hot(integer_encoded, depth=counter)
-    # One-hot encoding Y_int
-    # Y = tf.one_hot(Y_int, depth=2) # shape: (batch_size, max_len, 2)
+    # One-hot encoding
+    one_hot = tf.one_hot(encoded_input, depth=counter)
+    X = one_hot[:-1]
+    Y = one_hot[1:]
 
     session = tf.Session()
     session.run(tf.global_variables_initializer())
 
-    new_X = session.run(X)
+    new_X, new_Y = session.run([X, Y])
 
     # # #
     # use 16 blocks with subsequences of size 256. In that case, the input tensor dimension is (16, 256, k), where k is
@@ -96,6 +99,13 @@ with open(book, "r") as reader:
     batch_size = 16
     sequence_length = 256
 
-    batches = generate_batches(new_X, batch_size, sequence_length)
+    X_batches = generate_batches(new_X, batch_size, sequence_length, counter)
+    Y_batches = generate_batches(new_Y, batch_size, sequence_length, counter)
+
+    print(X_batches, Y_batches)
+
+print(counter)
+print(abs_freq)
+print(rel_freq)
 
     print(batches)
