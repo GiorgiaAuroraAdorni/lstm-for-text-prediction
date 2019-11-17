fadec03d6e295b7cf229067d3da28aed11ae5338
Giorgia Aurora Adorni <adornigiorgia@outlook.it>
2019-11-17 19:03:48 +0100

    Fixed download and preprocessing of file and some typos

--- src/lstm.py
+++ src/lstm.py
@@ -8,7 +8,7 @@ import requests
 import collections
 
 
-def generate_batches(input, batch_size, sequence_length, counter):
+def generate_batches(input, batch_size, sequence_length, k):
     """
     The text is too long to allow backpropagation through time, so it must be broken down into smaller sequences.
     In order to allow backpropagation for a batch of sequences, the text may first be broken down into a number of
@@ -22,14 +22,14 @@ def generate_batches(input, batch_size, sequence_length, counter):
     :param text: input sequence
     :param batch_size: number of blocks/batches in which divided the text
     :param sequence_length: the length of each subsequence of a block
-    :param counter:
+    :param k:
     :return batches: return the list of batches
     """
     block_length = input.shape[0] // batch_size
-    batches = np.array([]).reshape([0, batch_size, sequence_length, counter])
+    batches = np.array([]).reshape([0, batch_size, sequence_length, k])
 
     for i in range(0, block_length, sequence_length):
-        batch = np.array([]).reshape([0, sequence_length, counter])
+        batch = np.array([]).reshape([0, sequence_length, k])
         append = False
         for j in range(batch_size):
             start = j * block_length + i
@@ -59,31 +59,34 @@ if download:
     url = 'http://www.gutenberg.org/files/1184/1184-0.txt'
     r = requests.get(url, allow_redirects=True)
 
-    # Preprocess the text
-    # convert characters to lower case
-    text = r.text.lower()
-    text_file = open(book, 'w')
-    n = text_file.write(text)
-    text_file.close()
+    with open(book, 'wb') as text_file:
+        text_file.write(r.content)
 
-with open(book, "r") as reader:
-    # count the number of unique characters and the frequency of each character
+with open(book, "r", encoding='utf-8') as reader:
     input = reader.read()
-    input = np.array([c for c in input])
 
+    # Convert characters to lower case
+    input = input.lower()
+
+    # Count the number of unique characters and the frequency of each character
+    input = np.array([c for c in input])
     abs_freq = collections.Counter(input)
-    counter = len(abs_freq)
+    k = len(abs_freq)
     abs_freq = collections.OrderedDict(abs_freq)
     rel_freq = {key: value / len(input) for key, value in abs_freq.items()}
 
-    # choose one integer to represent each character
+    print(k)
+    print(abs_freq)
+    print(rel_freq)
+
+    # Choose one integer to represent each character
     char_to_int = {key: idx for idx, key in enumerate(abs_freq)}
     int_to_char = {idx: key for idx, key in enumerate(abs_freq)}
 
     encoded_input = [char_to_int[char] for char in input]
 
     # One-hot encoding
-    one_hot = tf.one_hot(encoded_input, depth=counter)
+    one_hot = tf.one_hot(encoded_input, depth=k)
     X = one_hot[:-1]
     Y = one_hot[1:]
 
@@ -92,20 +95,13 @@ with open(book, "r") as reader:
 
     new_X, new_Y = session.run([X, Y])
 
-    # # #
-    # use 16 blocks with subsequences of size 256. In that case, the input tensor dimension is (16, 256, k), where k is
-    # the number of unique characters (one-hot encoding).
-
+    # Truncated backpropagation through time: use 16 blocks with subsequences of size 256.
     batch_size = 16
     sequence_length = 256
 
-    X_batches = generate_batches(new_X, batch_size, sequence_length, counter)
-    Y_batches = generate_batches(new_Y, batch_size, sequence_length, counter)
+    X_batches = generate_batches(new_X, batch_size, sequence_length, k)
+    Y_batches = generate_batches(new_Y, batch_size, sequence_length, k)
 
     print(X_batches, Y_batches)
 
-print(counter)
-print(abs_freq)
-print(rel_freq)
 
-    print(batches)
